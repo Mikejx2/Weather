5b83a8cbca1f906b150c46840b99948eed683956
Michael Cook <michaeljcook53@gmail.com>
2021-05-10 15:02:50 -0400

    change

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-<script></script>
+
         let input  = prompt("Enter City ");
         document.querySelector('.userInput').innerHTML = input
-        var temp = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${APIKEY}`)
+        var temp = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=e02f93a3ef06f6d6e0563bf716805611`)
         // var temp = fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${input}&cnt=5&appid=d665c62c18e9b6b7723a986c6f3c83e1`)
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
             console.log(data.main)
 
-            let tem = data.main.temp;
+            let kel = data.main.temp;
             // let weat = data.weather.main;
             // let max = data.main.temp_max;
             // let min = data.main.temp_min;
